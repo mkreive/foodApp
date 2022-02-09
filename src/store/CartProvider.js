7823372e2a114b87dbcafda2114272f1cf8f20eb
mkreive <mkreive@gmail.com>
2022-02-09 11:53:26 +0200

    removing from cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -5,9 +5,11 @@ const defaultCartState = {
     items: [],
     totalAmount: 0,
 };
+
 const cartReducer = (state, action) => {
     if (action.type === "ADD") {
-        // const updatedItems = state.items.concat(action.item);
+        console.log(action);
+
         const updatetTotalAmount =
             state.totalAmount + action.item.price * action.item.amount;
 
@@ -31,6 +33,33 @@ const cartReducer = (state, action) => {
         return {
             items: updatedItems,
             totalAmount: updatetTotalAmount,
+        };
+    }
+
+    if (action.type === "REMOVE") {
+        console.log(action);
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(
+                (item) => item.id !== action.item.id
+            );
+        } else {
+            const updatedItem = {
+                ...existingItem,
+                amount: existingItem.amount - 1,
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
         };
     }
     return defaultCartState;
