df6d3879ae8a1003a21a63f432828dc2dd6c5064
mkreive <mkreive@gmail.com>
2022-02-21 16:01:07 +0200

    finished

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -56,11 +56,16 @@ const cartReducer = (state, action) => {
             };
             updatedItems = [...state.items];
             updatedItems[existingCartItemIndex] = updatedItem;
+
+            return {
+                items: updatedItems,
+                totalAmount: updatedTotalAmount,
+            };
         }
-        return {
-            items: updatedItems,
-            totalAmount: updatedTotalAmount,
-        };
+
+        if (action.type === "CLEAR") {
+            return defaultCartState;
+        }
     }
     return defaultCartState;
 };
@@ -79,11 +84,16 @@ const CartProvider = (props) => {
         dispatchCartAction({ type: "REMOVE", id: item.id });
     };
 
+    const clearCartHandler = () => {
+        dispatchCartAction({ type: "CLEAR" });
+    };
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
+        clearCart: clearCartHandler,
     };
     return (
         <CartContext.Provider value={cartContext}>
