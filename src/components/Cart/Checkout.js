1b581305b1cadd9358c3cb4d9bb8e8da7e9b84a8
mkreive <mkreive@gmail.com>
2022-02-21 13:18:55 +0200

    submiting cart data

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -45,6 +45,13 @@ const Checkout = (props) => {
         if (!formIsValid) {
             return;
         }
+
+        props.onConfirm({
+            name: enteredName,
+            street: enteredStreet,
+            city: enteredCity,
+            postal: enteredPostal,
+        });
     };
 
     const nameControlClasses = `${classes.control} ${
