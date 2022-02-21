70409300893351cc22558dce92566cafc26a9ff9
mkreive <mkreive@gmail.com>
2022-02-21 10:24:21 +0200

    database meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
@@ -33,7 +33,29 @@ const DUMMY_MEALS = [
 
 // Code
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => (
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch(
+                "https://react-http-e5ba5-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+            );
+            const responseData = await response.json();
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                });
+            }
+            setMeals(loadedMeals);
+        };
+        fetchMeals();
+    }, []);
+
+    const mealsList = meals.map((meal) => (
         <MealItem
             id={meal.id}
             key={meal.id}
